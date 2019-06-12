e76c5a8c64f0d7d26fbf6619af00bc6044768906
Quentin Barbarat <quentin.barbarat@epita.fr>
2019-06-12 16:55:00 +0200

    Adds initial version

---
+++ README.md
@@ -0,0 +1,13 @@
+## Larousse API
+
+### Why ?
+Because the Larousse website doesn't supply an api to look up definitions :(
+
+### How ?
+* python3
+* requests
+* re
+* BeautifulSoup
+
+### Possible improvements
+* Turn this into an actual pip package for ease of use
---
+++ larousse.py
@@ -0,0 +1,16 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+def get_definitions(word):
+    """
+    :param word: The word whose definition you are looking for
+    :return: A list containing all the definitions of word
+    """
+    url = "https://www.larousse.fr/dictionnaires/francais/" + word
+    soup = BeautifulSoup(requests.get(url=url).text, 'html.parser')
+    for ul in soup.find_all('ul'):
+        if ul.get('class') is not None and 'Definitions' in ul.get('class'):
+            return [re.sub("<.*?>", "", str(li)) for li in ul.find_all('li')]
+    return []
